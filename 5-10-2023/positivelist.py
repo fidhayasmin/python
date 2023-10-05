ba5b8bb611cb3677083364b17ae85ac7ec9593db
fidhayasmin <145666933+fidhayasmin@users.noreply.github.com>
2023-10-05 16:12:50 +0530

    Add files via upload

---
+++ 5-10-2023/positivelist.py
@@ -0,0 +1,8 @@
+a=[-20,30,50,-23]
+b=[]
+for i in a:
+    if i>0:
+       b.append(i)
+print(b)
+       
+
---
+++ 5-10-2023/vowels.py
@@ -0,0 +1,5 @@
+a="seventeen"
+b=["a","e","i","o","u","A","E","I","O","U"]
+for i in a:
+    if i in b:
+        print(i)
