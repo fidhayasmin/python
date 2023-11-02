cb19ebd27c2436e5d140e3692572ed9e9fbdd59f
fidhayasmin <145666933+fidhayasmin@users.noreply.github.com>
2023-11-02 16:07:33 +0530

    Add files via upload

---
+++ 26-10-2023/lambda.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the side of the square:\n"))
+area=lambda b:b*b
+print("area :",area(a))
+a=int(input("Enter the length of reactangle"))
+b=int(input("Enter the breadth of reactangle"))
+area=lambda l,b:1*b
+print("Area of the rectangle is",area(a,b))
+a=int(input("Enter the length of triangle"))
+b=int(input("Enter the breadth of triangle"))
+area=lambda h,b:h*b
+print("Area of the rectangle is",area(a,b))
+            
--- 26-10-2023/pyr.py
+++ 26-10-2023/pyr.py
@@ -3,7 +3,7 @@ def pyramid(n):
     for i in range(1,n+1):
         for n in range(1,i+1):
             print(n*i,end="")
-            print("\n")
+        print("\n")
 pyramid(n)
         
     
